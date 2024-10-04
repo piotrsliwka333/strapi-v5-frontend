366a1e7b7f49844b99b47f629f09c5475b913e65
Peter Sliwka <piotrsliwka333@gmail.com>
2024-10-04 16:11:16 +0200

    Main - Fix issue with NewsletterAPI

--- src/api/NewsletterAPI.ts
+++ src/api/NewsletterAPI.ts
@@ -1,10 +1,14 @@
-import { NewsletterCreateResponse, NewsletterCreateResponseError, NewsletterFindOneResponse, NewsletterPayload } from "@/models/Newsletter";
-import { HttpClient } from "./HttpClient";
+import {
+  NewsletterCreateResponse,
+  NewsletterCreateResponseError,
+  NewsletterFindOneResponse,
+  NewsletterPayload,
+} from '@/models/Newsletter';
+import { HttpClient } from './HttpClient';
 
 export class NewsletterAPI {
-
   static getUrl(id?: string) {
-    return id ? `/newsletter-users${id}` : `/newsletter-users`
+    return id ? `/newsletter-users/${id}` : `/newsletter-users`;
   }
 
   static getPayload(payload: NewsletterPayload) {
@@ -12,18 +16,31 @@ export class NewsletterAPI {
   }
 
   static findOne(id: string): Promise<NewsletterFindOneResponse | NewsletterCreateResponseError> {
-    return HttpClient.get(this.getUrl(id)).then(response => HttpClient.mapResponse<NewsletterFindOneResponse | NewsletterCreateResponseError>(response))
+    return HttpClient.get(this.getUrl(id)).then((response) =>
+      HttpClient.mapResponse<NewsletterFindOneResponse | NewsletterCreateResponseError>(response)
+    );
   }
 
-  static create(payload: NewsletterPayload): Promise<NewsletterCreateResponse | NewsletterCreateResponseError> {
-    return HttpClient.post(this.getUrl(), this.getPayload(payload)).then(response => HttpClient.mapResponse<NewsletterCreateResponse | NewsletterCreateResponseError>(response))
+  static create(
+    payload: NewsletterPayload
+  ): Promise<NewsletterCreateResponse | NewsletterCreateResponseError> {
+    return HttpClient.post(this.getUrl(), this.getPayload(payload)).then((response) =>
+      HttpClient.mapResponse<NewsletterCreateResponse | NewsletterCreateResponseError>(response)
+    );
   }
 
-  static update(id: string, payload: NewsletterPayload): Promise<NewsletterCreateResponse | NewsletterCreateResponseError> {
-    return HttpClient.put(this.getUrl(id), this.getPayload(payload)).then(response => HttpClient.mapResponse<NewsletterCreateResponse | NewsletterCreateResponseError>(response))
+  static update(
+    id: string,
+    payload: NewsletterPayload
+  ): Promise<NewsletterCreateResponse | NewsletterCreateResponseError> {
+    return HttpClient.put(this.getUrl(id), this.getPayload(payload)).then((response) =>
+      HttpClient.mapResponse<NewsletterCreateResponse | NewsletterCreateResponseError>(response)
+    );
   }
 
   static delete(id: string): Promise<NewsletterCreateResponse | NewsletterCreateResponseError> {
-    return HttpClient.delete(this.getUrl(id)).then(response => HttpClient.mapResponse<NewsletterCreateResponse | NewsletterCreateResponseError>(response))
+    return HttpClient.delete(this.getUrl(id)).then((response) =>
+      HttpClient.mapResponse<NewsletterCreateResponse | NewsletterCreateResponseError>(response)
+    );
   }
 }
